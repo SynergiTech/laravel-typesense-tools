ab46e1b4d057782cae3797a2f4d9d43b8a34ae0f
William Hall <william.hall@synergitech.co.uk>
2026-03-26 13:43:11 +0000

    📚 document an initial migration hack

--- examples/models/User.php
+++ examples/models/User.php
@@ -15,6 +15,11 @@ class User
 
     public static function indexableAs(): string
     {
+        // inspiration for an initial migration hack, remove when aliases are in use
+        if (env('APP_ENV') !== 'local' && env('TYPESENSE_VERSION_SUFFIX') === null) {
+            return self::searchableAs();
+        }
+
         // @phpstan-ignore larastan.noEnvCallsOutsideOfConfig
         return self::searchableAs() . '_' . env('TYPESENSE_VERSION_SUFFIX', 'default');
     }
